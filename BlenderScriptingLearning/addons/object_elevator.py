ee6524ff080cf9777bdf0e3ef2db9f58f0e29df2
umardas12 <umard8701@gmail.com>
2024-09-20 19:23:23 +0530

    adding sort based on parent hierarchy

--- BlenderScriptingLearning/addons/object_elevator.py
+++ BlenderScriptingLearning/addons/object_elevator.py
@@ -10,18 +10,30 @@ bl_info = {
 
 import bpy
 from bpy.props import FloatProperty
+from copy import copy
+from bpy.props import BoolProperty
 
 class OBJECT_OT_elevator(bpy.types.Operator):
     bl_idname = "object.pckt_floor_transform"
     bl_label = "Elevator Objects"
     bl_options = {'REGISTER','UNDO'}
     floor: FloatProperty(name="Floor", default=0)
+    constr: BoolProperty(name="Constraints", default=False)
 
     @classmethod
     def poll(cls,context):
         return len(bpy.context.selected_objects)>0
     
+    def ancestors_count(self,ob):
+        ancestors = 0
+        while ob.parent:
+            ancestors += 1
+            ob = ob.parent
+        return ancestors
+    
     def execute(self,context):
+        selected_objects = copy(context.selected_objects)
+        selected_objects.sort(key = self.ancestors_count)
         for ob in context.selected_objects:
             matrix_world = ob.matrix_world
             #if ob.location.z > self.floor:
